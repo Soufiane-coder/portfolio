8491e573867de5574b0b8aa94d2e4dcd1d92c3cf
soufiane-coder <soufiane.amimi@um5r.ac.ma>
2025-06-02 22:18:14 +0100

    changing logo

--- src/components/logo.component.tsx
+++ src/components/logo.component.tsx
@@ -1,20 +1,50 @@
-"use client"
+'use client'
+
+import { useContext, useEffect, useState } from 'react'
 import Link from 'next/link'
-import React from 'react'
 import { motion } from 'framer-motion'
-import OakLogo from '@/../public/oak-logo.svg'
 import Image from 'next/image'
+import BonsaiiLogo from '@/../public/images/bonsaii-logo-nobackground-center.svg'
+import BonsaiiLogoWhite from '@/../public/images/bonsaii-logo-nobackground-center-white.svg'
+import ThemeContext from '@/app/theme.context'
+import UseThemeSwitcher from './hooks/use-theme-switcher.hook'
 
 const MotionLink = motion(Link)
+
 const Logo = () => {
+  const [logoSrc, setLogoSrc] = useState(BonsaiiLogo)
+  const [mode] = UseThemeSwitcher()
+  const context = useContext(ThemeContext)
+
+  if (!context) {
+    throw new Error('Logo must be used within ThemeContext Provider')
+  }
+
+  const { contextValue } = context
+
+  useEffect(() => {
+    if (contextValue === 'dark') {
+      setLogoSrc(BonsaiiLogoWhite)
+    } else {
+      setLogoSrc(BonsaiiLogo)
+    }
+  }, [contextValue])
+
   return (
-    <div>
-      <div className="flex items-center justify-center mt-2">
-        <MotionLink href='https://oakcorporation.org' className='w-16 h-16 p-4 dark:bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold border border-solid border-transparent border-gray-900 dark:border-light '
-        >
-          <Image src={OakLogo} alt="oak" className='fill-white'/>
-        </MotionLink>
-      </div>
+    <div className="flex items-center justify-center mt-2">
+      <MotionLink
+        target='_blank'
+        href="https://bonsaii.org"
+        className="w-16 h-16 dark:bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold border border-solid border-transparent"
+      >
+        <Image
+          src={logoSrc}
+          alt="Oak Logo"
+          width={64}
+          height={64}
+          priority
+        />
+      </MotionLink>
     </div>
   )
 }
